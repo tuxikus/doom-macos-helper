7fb8f083eb44f2af08f2157a893c3c1cc0603535
tuxikus <contact@tuxikus.de>
2025-11-10 12:03:44 +0100

    fix

--- config.el
+++ config.el
@@ -1,12 +1,10 @@
-;;; os/macos-helper/open-in-cursor.el -*- lexical-binding t -*-
+;;; os/macos-helper/config.el -*- lexical-binding t -*-
 
-;;;###autoload
 (defun +macos-helper/open-current-file-in-cursor ()
   (interactive)
   (let ((path (buffer-file-name)))
     (shell-command (concat "cursor " path))))
 
-;;;###autoload
 (defun +macos-helper/open-current-project-in-cursor ()
   (interactive)
   (let ((project (projectile-project-root)))
